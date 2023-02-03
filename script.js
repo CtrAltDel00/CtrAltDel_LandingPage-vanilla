f59ba3ea804d2e730ef4b55cea739bd832b5b2ec
martin nkemakolam <martinnkemakolam@gmail.com>
2023-02-03 13:58:56 -0800

    still not final

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 var currentIndex = 0; // Declare and initialize the current index
 var cards = document.querySelectorAll('.card');
-setTimeout(showNextCard, 5000);
-
-function showNextCard() {
-  // Check if the current index is the last card
-  if (currentIndex === cards.length - 1) {
-    currentIndex = 0;
-  } else {
-    currentIndex++;
-  }
-
-  // Hide all cards
-  for (let i = 0; i < cards.length; i++) {
-    cards[i].style.display = "none";
-  }
-
-  // Show the current card
-  cards[currentIndex].style.display = "block";
-  setTimeout(showNextCard, 5000);
+let topBtn = document.querySelector('#topButton')
+let list = document.querySelector('.list')
+let childrenObs = list.children
+let current = []
+function classAdder() {
+    if (document.querySelector('.container').clientWidth < 800) {
+        return 
+    }else{
+        current[0].classList.add('active')
+        cards.forEach((item, id)=>{
+            if (currentIndex === id) {
+                item.classList.add('active')
+                item.classList.add("animateAdd")
+                current.push(item)
+            }else{
+                item.classList.remove('active')
+            }
+        })   
+    }
 }
+
+let currentMover = setInterval(()=>{
+    if (currentIndex === 2) {
+        currentIndex = -1
+    }
+    currentIndex = currentIndex + 1
+    classAdder()
+}, 5000)
